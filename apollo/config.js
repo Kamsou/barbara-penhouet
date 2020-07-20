44ea203ef1f443943ec56162e99634c4ac020d68
Camille <camille.coutens@gmail.com>
2020-07-20 13:19:31 +0200

    test DatCMS

---
+++ apollo/config.js
@@ -0,0 +1,6 @@
+export default function(context) {
+  return {
+    httpEndpoint: 'https://graphql.datocms.com',
+    getAuth: () => '095c38f9f0cea84f784f3f573e5606'
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2 v-for="(contact, index) in allContacts" :key="index">
+      {{contact.paragraphe}}
+    </h2>
+  </div>
+</template>
+
+<script>
+
+  import gql from 'graphql-tag'
+
+  export default {
+    apollo: {
+      allContacts: gql`{
+        allContacts {
+          paragraphe
+        }
+      }`
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
