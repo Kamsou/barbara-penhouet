5de23d22125ba61fd27ea9b7724261df330896ea
Camille <camille.coutens@gmail.com>
2020-08-13 19:22:37 +0200

    install snipcart

--- pages/teeshirt/_slug.vue
+++ pages/teeshirt/_slug.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <h1>Banzai</h1>
+    <div
+    :data-item-id="product.sku"
+    :data-item-name="product.name"
+    :data-item-price="product.price"
+    :data-item-image="product.image"
+    :data-item-url="`https://snipcart-nuxt-pwa.netlify.com/`"
+    >
+    </div>
   </div>
 </template>
 
