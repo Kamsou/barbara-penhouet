4392e97ee3a94e3df10c49f74b243e39a9a0b108
Camille <camille.coutens@gmail.com>
2020-08-13 19:25:31 +0200

    first commit

--- pages/teeshirt/_slug.vue
+++ pages/teeshirt/_slug.vue
@@ -3,7 +3,7 @@
     <div
     :data-item-id="product.sku"
     :data-item-name="product.name"
-    :data-item-price="product.price"
+    :data-item-price="product.price" 
     :data-item-image="product.image"
     :data-item-url="`https://snipcart-nuxt-pwa.netlify.com/`"
     >
