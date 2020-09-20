4f0e9af351240dfa8fa046bc61115845bb409727
Camille <camille.coutens@gmail.com>
2020-09-20 20:54:58 +0200

    animation menu

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -6,10 +6,18 @@
       alt="Barbara Penhouet"
     />
     <div class="navbar_menu">
-      <span>Painter</span>
-      <span>Illustrations</span>
-      <span>Interior</span>
-      <span>Shop</span>
+      <div>
+        <span data-text="Painter">Painter</span>
+      </div>
+      <div>
+        <span data-text="Illustrations">Illustrations</span>
+      </div>
+      <div>
+        <span data-text="Interior">Interior</span>
+      </div>
+      <div>
+        <span data-text="Shop">Shop</span>
+      </div>
       <img
         class="navbar_mail"
         src="/images/mail.svg"
@@ -41,6 +49,23 @@
     align-items: center;
     justify-content: space-around;
     padding-left: 50px;
+    div {
+      overflow: hidden;
+      &:hover span {
+        transform: translateY(-100%);
+        transition: 0.5s;
+      }
+      span {
+        display: inline-block;
+        transition: 0.5s;
+        transform: translateY(0);
+        &::before {
+          content: attr(data-text);
+          position: absolute;
+          transform: translateY(100%);
+        }
+      }
+    }
   }
 }
 </style>
