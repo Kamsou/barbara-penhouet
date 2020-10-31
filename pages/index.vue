5cf36fdc34e3a83e3c33ac38362163774a0a54a3
Camille <camille.coutens@gmail.com>
2020-10-31 20:00:28 +0100

    ADD: Desktop ok

--- pages/index.vue
+++ pages/index.vue
@@ -68,6 +68,40 @@
         </div>
       </div>
     </section>
+    <section class="four-bloc-index">
+      <div class="superposition">
+        <img class="img-01" src="/images/index/homme-plongeon.png" alt="homme qui plonge">
+        <img class="img-02" src="/images/index/illustrations-animations.png" alt="illustrations et animations">
+      </div>
+    </section>
+    <section class="five-bloc-index">
+      <div class="superposition">
+        <img class="img-01" src="/images/index/interieur.png" alt="interieur">
+        <img class="img-02" src="/images/index/salon.png" alt="un salon dessiné">
+        <img class="img-03" src="/images/index/objet-design.png" alt="un objet design">
+      </div>
+    </section>
+    <section class="six-bloc-index">
+      <div class="superposition">
+        <img class="img-01" src="/images/index/tee-shirt.png" alt="tee-shirt">
+        <img class="img-02" src="/images/index/shop.png" alt="shop">
+      </div>
+    </section>
+    <section class="seven-bloc-index">
+      <div class="superposition">
+        <img class="img-01" src="/images/index/femme-allongee.png" alt="une femme allongée">
+        <div class="footer">
+          <div class="links">
+            <span>Contact</span>
+            <span>Facebook</span>
+            <span>Instagram</span>
+          </div>
+          <div class="arrow-up">
+            <img src="/images/index/arrow.svg" alt="">
+          </div>
+        </div>
+      </div>
+    </section>
     <!-- <div style="margin-top: 150px; text-align: center">
       <p>Produit 1</p>
       <nuxt-link :to="`/teeshirt/produit1`" style="border: 1px solid #000; width: 20%; margin: 0 auto">
@@ -166,7 +200,7 @@
   .three-bloc-index {
     display: flex;
     padding: 0 17vw 0 2.5vw;
-    height: 100vh;
+    height: 44.653vw;
     .images {
       width: 50%;
       position: relative;
@@ -197,6 +231,121 @@
         .index-lign-flex {
           display: flex;
           justify-content: space-between;
+        }
+      }
+    }
+  }
+  .four-bloc-index {
+    height: 54.583vw;
+    display: block;
+    padding: 0 5.486vw 0 0;
+    .superposition {
+      position: relative;
+      display: flex;
+      justify-content: flex-end;
+      height: 100%;
+      .img-01 {
+        position: absolute;
+        top: 19%;
+        right: 11%;
+        width: 32.361vw;
+        z-index: 1;
+      }
+      .img-02 {
+        position: absolute;
+        top: 0;
+        width: 34.722vw;
+        z-index: 2;
+      }
+    }
+  }
+  .five-bloc-index {
+    height: 54.653vw;
+    display: block;
+    padding: 5% 0 0 2.361vw;
+    .superposition {
+      position: relative;
+      display: flex;
+      justify-content: flex-start;
+      height: 100%;
+      .img-01 {
+        position: absolute;
+        width: 30.625vw;
+        z-index: 2;
+        bottom: 70%;
+      }
+      .img-02 {
+        position: absolute;
+        left: 11%;
+        width: 28.472vw;
+        z-index: 1;
+      }
+      .img-03 {
+        position: absolute;
+        top: 33%;
+        left: -2%;
+        width: 25.764vw;
+        z-index: 3;
+      }
+    }
+  }
+  .six-bloc-index {
+    height: 32vw;
+    display: block;
+    padding: 0 7.778vw 0 0;
+    .superposition {
+      position: relative;
+      display: flex;
+      justify-content: flex-end;
+      height: 100%;
+      .img-01 {
+        position: absolute;
+        width: 41.528vw;
+        z-index: 1;
+      }
+      .img-02 {
+        position: absolute;
+        width: 26.736vw;
+        top: 80%;
+        right: 16%;
+        z-index: 2;
+      }
+    }
+  }
+  .seven-bloc-index {
+    height: 101.389vw;
+    display: block;
+    padding: 0 0 7.014vw 7.569vw;
+    .superposition {
+      position: relative;
+      display: flex;
+      justify-content: flex-end;
+      height: 100%;
+      .img-01 {
+        position: absolute;
+        width: 100vw;
+        z-index: 1;
+      }
+      .footer {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
+        width: 100%;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        z-index: 2;
+        .links {
+          span {
+            display: block;
+            width: 34.722vw;
+            font-size: 2.639vw;
+            font-weight: bold;
+            padding-bottom: 15px;
+          }
+        }
+        .arrow-up {
+          margin-right: 10.278vw;
         }
       }
     }
